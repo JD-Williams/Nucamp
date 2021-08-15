c4b8124f5f81e893880fcd04ad30bbef3cb1a877
Jonathan D. Williams <jdwill917@live.com>
2021-08-15 11:52:58 -0400

    Added solution to Wk02

---
+++ 1-Fundamentals/assignments/wk02/solution1/banking_pkg/account.py
@@ -0,0 +1,19 @@
+#========================= 
+# TASK 4
+#=========================
+
+def show_balance(balance):
+    print(f"Current Balance: ${balance}")
+
+def deposit(balance):
+    amount = float(input("Enter amount to deposit: "))
+    balance += amount
+    return balance
+
+def withdraw(balance):
+    amount = float(input("Enter amount to withdraw: "))
+    balance -= amount
+    return balance
+
+def logout(name):
+    print(f"Goodbye {name}!")
